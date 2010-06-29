334743878e5732ad7877c19b03994af6d3abdced
Chas Lemley <chas.lemley@gmail.com>
2010-06-29 17:03:20 -0400

    second commit

--- green_scrape_sinatra.rb
+++ green_scrape_sinatra.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'nokogiri'
 require 'open-uri'
+require 'haml'
 
 # url = 'http://boatingbeta.com/runs/flows/green'
 # doc = Nokogiri::HTML(open(url))
@@ -19,10 +20,21 @@ get '/' do
   
   text = doc.at_css("p:nth-child(7)")
   
-  message = "Green Schedule for Next 3 days" << "<br />"
-  message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "<br />"
-  message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "<br />"
-  message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "<br />"
+  @message = "Green Schedule for Next 3 days" << "\n"
+  @message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "\n"
+  @message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "\n"
+  @message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "\n"
   
-  message
+  @message
+  
+  haml :index
 end
+
+__END__
+
+@@ index
+!!! XML
+%Response
+  %Sms
+    = @message
+
