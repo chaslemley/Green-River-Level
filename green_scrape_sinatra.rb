3bac07550f7811cd5447c24c2fd68dbefcd3b2b9
Chas Lemley <chas.lemley@gmail.com>
2010-06-29 16:53:15 -0400

    first commit

--- green_scrape_sinatra.rb
+++ green_scrape_sinatra.rb
@@ -13,6 +13,16 @@ require 'open-uri'
 # puts "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}"
 # puts "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}"
 
-get '/'
- 'ahh'
+get '/' do
+  url = 'http://boatingbeta.com/runs/flows/green'
+  doc = Nokogiri::HTML(open(url))
+  
+  text = doc.at_css("p:nth-child(7)")
+  
+  message = "Green Schedule for Next 3 days" << "<br />"
+  message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "<br />"
+  message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "<br />"
+  message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "<br />"
+  
+  message
 end
