10c315f8b3c89c98f8de42c02ff6f3b87ab06256
Chas Lemley <chas.lemley@gmail.com>
2010-06-29 23:03:08 -0400

    work

--- green_scrape_sinatra.rb
+++ green_scrape_sinatra.rb
@@ -20,10 +20,10 @@ get '/' do
   
   text = doc.at_css("p:nth-child(7)")
   
-  @message = "Green Schedule for Next 3 days" << "\n"
-  @message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "\n"
-  @message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "\n"
-  @message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "\n"
+  @message = "Green Schedule" << "\n"
+  @message << "(#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "\n"
+  @message << "(#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "\n"
+  @message << "(#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "\n"
   
   @message
   content_type 'application/xml'
