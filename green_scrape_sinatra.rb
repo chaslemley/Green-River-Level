4fe95859c9e9472f72cbb584bf8887c8b3241263
Chas Lemley <chas.lemley@gmail.com>
2010-06-29 23:01:28 -0400

    work

--- green_scrape_sinatra.rb
+++ green_scrape_sinatra.rb
@@ -26,6 +26,7 @@ get '/' do
   @message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "\n"
   
   @message
+  content_type 'application/xml'
   
   haml :index
 end
@@ -37,9 +38,9 @@ post '/' do
   text = doc.at_css("p:nth-child(7)")
   
   @message = "Green Schedule for Next 3 days" << "\n"
-  @message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}): #{text.children[2].text[27..-2]}" << "\n"
-  @message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}): #{text.children[4].text[27..-2]}" << "\n"
-  @message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}): #{text.children[6].text[27..-2]}" << "\n"
+  @message << "Today (#{Time.parse(text.children[2].text[0..9]).strftime('%a')}) #{text.children[2].text[27..-2]}" << "\n"
+  @message << "Tomorrow (#{Time.parse(text.children[4].text[0..9]).strftime('%a')}) #{text.children[4].text[27..-2]}" << "\n"
+  @message << "In 2 days (#{Time.parse(text.children[6].text[0..9]).strftime('%a')}) #{text.children[6].text[27..-2]}" << "\n"
   
   @message
   content_type 'application/xml'
