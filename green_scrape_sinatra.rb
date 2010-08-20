65722f4a4d809cc0f8d18e835aec196be6e68a40
Chas Lemley <chas.lemley@gmail.com>
2010-08-19 20:31:22 -0400

    fake get

--- green_scrape_sinatra.rb
+++ green_scrape_sinatra.rb
@@ -27,6 +27,29 @@ post '/' do
   haml :index
 end
 
+get '/' do
+  doc = Nokogiri::HTML(open('http://boatingbeta.com/runs/flows/green'))
+  
+  text = doc.at_css("p:nth-child(7)")
+    
+  @message = "Green Schedule" << "\n"
+  
+  [2,4,6].each do |index|
+    if text.children[index]
+      day_of_week = Time.parse(text.children[index].text[0..9]).strftime('%a')
+      release_text = text.children[index].text[14..-2]
+      @message << "(#{day_of_week}) #{release_text}" << "\n"
+    end
+  end
+  
+  lake_level = "99.5"
+  
+  @message << "Lake Level: #{lake_level}"
+
+  content_type 'application/xml'
+  haml :index
+end
+
 __END__
 
 @@ index
